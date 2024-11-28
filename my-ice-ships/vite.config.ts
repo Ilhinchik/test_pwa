090a442d4858b74733898e11cc141029f5209cf3
Ilya <markovila539@gmail.com>
2024-11-28 21:42:17 +0000

    add pwa

--- my-ice-ships/vite.config.ts
+++ my-ice-ships/vite.config.ts
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA, VitePWAOptions} from "vite-plugin-pwa";
+
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+        name: 'Заявки на проводку кораблей',
+        short_name: 'ЗПК',
+        description: 'Описание',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png'
+            }
+        ]
+    }
+};
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(manifestForPlugin)
+  ],
   base: "/Ice_ships_frontend",
   server: {
       port: 3000,
